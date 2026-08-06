dd16e5abf997c2366cb0ef9b669c326d1c7e4620
jsmontes-55 <juansebastianmontesprieto@gmail.com>
2026-08-06 12:18:06 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Este archivo convierte la carpeta "EmotionDetection" en un paquete de Python.
+# Al importar desde aquí la función emotion_detector, el usuario del paquete
+# puede escribir simplemente:
+#     from EmotionDetection import emotion_detector
+# en lugar de:
+#     from EmotionDetection.emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
